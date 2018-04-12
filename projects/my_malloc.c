a6ca44da70f778ae5933f0d4bbba9539b5a4e233
IanSweeney <imsweene@calpoly.edu>
2018-04-11 21:18:34 -0700

    First 'finished' commit still have to fully implement dbg_flg

--- projects/my_malloc.c
+++ projects/my_malloc.c
@@ -2,81 +2,99 @@
 #include <unistd.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 #include <errno.h>
 
-#define HEAP_INCREMENT (64*1024)
-#define HEAD_SIZE 20
 
+#define ADDR_WIDTH 8
+#define HEAP_INCREMENT (64*1024)
+/* #define HEAD_SIZE (sizeof(struct memblk_s)) */
+#define HEAD_SIZE 32
+
+#define align_16(x) (((((x)-1)>>4)<<4)+16)
+
+
+/* Memory block structure */
+typedef struct memblk_s {
+	size_t size;
+	int free;
+	struct memblk_s * prev;
+	struct memblk_s * next;
+	char data[1];
+} memblk;
+
+
+/* Function prototypes */
 void *calloc(size_t nmemb, size_t size);
 void *malloc(size_t size);
 void free(void *ptr);
 void *realloc(void *ptr, size_t size);
- 
-typedef struct memblk_s {
-	size_t size;
-	int free;
-	struct memblk_s * next;
-} memblk;
 
+memblk *split_block(memblk *ptr, size_t size);
+memblk *merge_blocks(memblk *ptr, memblk *next);
+memblk *find_block(void *ptr);
+void print_list();
+
+
+/* Global variables */
 memblk *head = NULL;
-memblk *end = NULL;
 
+
+/* Malloc family */
 void *malloc(size_t size) {
-	/* Need to align*/
 	char* dbg = getenv("DEBUG_MALLOC");
+	int dbg_flg = 0;
 
+	size_t asize = align_16(size);
 	memblk *cur = head;
+
 	/* If this is the first malloc*/
 	if (head == NULL) {
+		
 		/* Create the linked list */
 		if ((head = sbrk(0)) == -1) {
 			/* Err msg */
 			errno = ENOMEM;
 			return NULL;
 		}
+
+		/* Align the head */
+		uintptr_t p = head;
+		head = (memblk*) align_16(p);
+
 		/* Allocate a large block of memory */
-		while (head + HEAD_SIZE + size > sbrk(0)) {
+		while (head->data + asize > sbrk(0)) {
 			/* Extend heap */
 			if (sbrk(HEAP_INCREMENT) == -1) {
 				/* Err msg */
 				errno = ENOMEM;
 				return NULL;
 			}
-		}	
+		}
 
 		/* Finish creating the head */
 		head->free = 0;
-		head->size = size; /* Need to align size */
-		head->next = head + HEAD_SIZE + size;
-		end = head->next;
-		end->free = 1;
+		head->size = asize;
+		head->next = NULL;
+		head->prev = NULL;
 
-		/* Pointer to where data goes */
-		cur = head + HEAD_SIZE;
+		/* Pointer to header for new data */
+		cur = head;
 	}
 	/* Otherwise find the next available memory */
 	else {
-		while (cur->free == 0 || cur->size < size) {
+		while (cur->next && (cur->free == 0 || cur->size < asize)) {
 			cur = cur->next;
 		}
 
-		if (cur != end) {
-			/* Reallocating a freed block */
-			if (cur->size - size > HEAD_SIZE + 1) {
-				/* make more free space */
-				memblk *temp = cur->next;
-				cur->next = cur + HEAD_SIZE + size;
-				cur->next->size = cur->size - size - HEAD_SIZE;
-				cur->next->free = 1;
-				cur->next->next = temp;
-			}
-
-			cur->size = size;
-			cur->free = 0;
+		/* Reallocating a freed block */
+		if (cur->next) {
+			cur = split_block(cur, asize);
 		}
 
+		/* Creating a new block */
 		else {
-			while (cur + HEAD_SIZE + size > sbrk(0)) {
+			while (cur + cur->size + asize + 2*HEAD_SIZE > sbrk(0)) {
 				/* Extend heap */
 				if (sbrk(HEAP_INCREMENT) == -1) {
 					/* Err msg */
@@ -86,37 +104,206 @@ void *malloc(size_t size) {
 			}
 
 			/* Add a Node to the list */
-			cur->size = size;
+			cur->next = (memblk*) (cur->data + cur->size);
+			cur->next->prev = cur;
+			cur = cur->next; /* move ptr to new block */
+			cur->size = asize;
 			cur->free = 0;
-			cur->next = cur + HEAD_SIZE + size;
-			end = cur->next;
+			cur->next = NULL;
+			/*end = cur->next;*/
 		}
 
-		/* Pointer to where data goes */
-		cur = cur + HEAD_SIZE;
+	}
+	
+	if (dbg_flg) {
+		/* MALLOC: malloc(%d) => (ptr=%p, size=%d) */
+		char str[80] = {"\0"};
+		int msg_size = 80;
+
+		snprintf(str, msg_size, 
+			"MALLOC: malloc(%d)		=> (ptr=%p, size=%d)\n\r", 
+			(int)size, cur, (int)asize);
+		write(STDERR_FILENO, str, msg_size);
 	}
 
-	/* MALLOC: malloc(%d) => (ptr=%p, size=%d) */
-	char str[80] = {"\0"};
-	int msg_size = 80;
-	
-	snprintf(str, msg_size, 
-		"MALLOC: malloc(%d)	=> (ptr=%p, size=%d)\n\r", 
-		(int)size, cur, (int)size);
-	write(STDOUT_FILENO, str, msg_size);
-	return cur;
+	return cur->data;
 }
 
 void *calloc(size_t nmemb, size_t size) {
-	return NULL;
+
+	char* dbg = getenv("DEBUG_MALLOC");
+	int dbg_flg = 0;
+	
+	size_t asize = align_16(nmemb * size);
+	if (asize == 0) {
+		return NULL;
+	}
+
+	memblk *cur = malloc(asize);
+	if (cur == NULL) {
+		return NULL;
+	}
+
+	if (dbg_flg) {
+		/* MALLOC: malloc(%d) => (ptr=%p, size=%d) */
+		char str[80] = {"\0"};
+		int msg_size = 80;
+
+		snprintf(str, msg_size, 
+			"MALLOC: calloc(%d,%d)	=> (ptr=%p, size=%d)\n\r", 
+			(int)nmemb, (int)size, cur, (int)asize);
+		write(STDERR_FILENO, str, msg_size);
+	}
+	
+	return memset(cur->data, 0, asize);
 }
 
 void free(void *ptr) {
+
+	char* dbg = getenv("DEBUG_MALLOC");
+	int dbg_flg = 0;
+	
+
+	if (ptr == NULL) {
+		return;
+	}
+
+	memblk *cur = find_block(ptr);
+	if (!cur) {
+		return;
+	}
+
+	else {
+		cur->free = 1;
+		
+		/* combine with prev if free */
+		if (cur->prev && cur->prev->free) {
+			cur = merge_blocks(cur->prev, cur);
+			cur = cur->next;
+		}
+
+		/* combine with next if free */
+		if (cur->next && cur->next->free) {
+			cur = merge_blocks(cur, cur->next);	
+		}
+	}
+
+	if (dbg_flg) {
+		/* MALLOC: free(%p) */
+		char str[80] = {"\0"};
+		int msg_size = 80;
+		
+		snprintf(str, msg_size, "MALLOC: free(%p)\n\r", cur);
+		write(STDERR_FILENO, str, msg_size);
+	}
+
 	return;
 }
 
 void *realloc(void *ptr, size_t size) {
-	return NULL;
+
+	char* dbg = getenv("DEBUG_MALLOC");
+	int dbg_flg = 0;
+
+	size_t asize = align_16(size);
+
+	memblk *cur = find_block(ptr);
+	if (!cur) {
+		/* Need to not print twice */
+		return malloc(size);
+	}
+	else if (size == 0) {
+		/* Need to not print twice */
+		free(ptr);
+		return NULL;
+	}
+
+
+	if (asize <= cur->size) {
+		cur = split_block(cur, asize);
+	}
+
+	else {
+		/* Try to extend the block */
+		if(cur->next && cur->next->free && 
+			(cur->size + cur->next->size + HEAD_SIZE) > asize) {
+			cur = merge_blocks(cur, cur->next);
+			cur = split_block(cur, asize);
+		}
+				
+		/* Malloc and copy data */ 
+		else {
+			memblk *temp = cur;
+			if ((cur = malloc(asize)) == NULL) {
+				return NULL;
+			}
+			memcpy(cur->data, temp->data, asize);
+			/* Need to not print twice */
+			free(temp);
+		}
+	}
+
+	
+	if (dbg_flg) {
+		/* MALLOC: realloc(%p,%d) => (ptr=%p, size=%d) */
+		char str[80] = {"\0"};
+		int msg_size = 80;
+		
+		snprintf(str, msg_size, 
+			"MALLOC: realloc(%p,%d)	=> (ptr=%p, size=%d)\n\r", 
+			ptr, (int)size, cur, (int)asize);
+		write(STDERR_FILENO, str, msg_size);
+	}
+	
+	return cur;
+}
+
+
+/* Helper functions */
+memblk *split_block(memblk *ptr, size_t size) {
+	if (ptr->size - size > HEAD_SIZE + 1) {
+		/* make more free space */
+		memblk *temp = ptr->next;
+		ptr->next = ptr->data + size;
+		ptr->next->size = ptr->size - size - HEAD_SIZE;
+		ptr->next->free = 1;
+		ptr->next->next = temp;
+		temp->prev = ptr->next;
+
+		/* So memory blocks smaller than HEAD_SIZE */ 
+		/* don't get abbandoned */
+		ptr->size = size;
+	}
+
+	ptr->free = 0;
+
+	return ptr;
+}
+
+memblk *merge_blocks(memblk *ptr, memblk *next) {
+	ptr->size += next->size + HEAD_SIZE;
+	if (next->next) {
+		ptr->next = next->next;
+		next->next->prev = ptr;
+	}
+	else {
+		ptr->next = NULL;
+	}
+
+	return ptr;
+}
+
+memblk *find_block(void *ptr) {
+	if (ptr == NULL) {
+		return NULL;
+	}
+
+	memblk *cur = head;
+	while (cur && ((cur->data + cur->size) < ptr)) {
+		cur = cur->next;
+	}
+
+	return cur;
 }
 
 void print_list() {
@@ -124,12 +311,12 @@ void print_list() {
 
 	char str[80] = {"\0"};
 	int msg_size = 80;
-	
+
 	while (view != NULL) {
 		snprintf(str, msg_size, 
 			"(ptr=%p, size=%d, free=%d)\n", 
 			view, (int)view->size, view->free);
-		write(STDOUT_FILENO, str, msg_size);
+		write(STDERR_FILENO, str, msg_size);
 
 		view = view->next;
 	}
--- projects/tests.c
+++ projects/tests.c
@@ -1,12 +1,91 @@
 /* Test script for my malloc */
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+
+int t1_general();
+int t2_defrag();
+int t3_realloc();
+
+void p_print(void* p);
+void s_print(char *s);
 
 int main(int argc, char* argv[]) {
-	void* p;
-	p = malloc(1);
-	void *p2 = malloc(2);
+	// p_print(sbrk(0));
+	// sbrk(1);
+	// p_print(sbrk(0));
+	
 
-	print_list();
+	// t2_defrag();
+	// t3_realloc();
+	
+	// p_print(sbrk(0));
+
+	// print_list();
 	return 0;
+}
+
+int t1_general() {
+	return 1;
+}
+
+int t2_defrag() {
+	s_print("running t2_defrag");
+
+	void *p1 = malloc(64);
+	void *p2 = malloc(128);
+	void *p3 = malloc(512);
+	void *p4 = malloc(256);
+	void *p5 = malloc(1010);
+
+	free(p5);
+	print_list();
+	free(p4);
+	print_list();
+	free(p3);
+	print_list();
+	free(p2);
+	print_list();
+	free(p1);
+	print_list();
+
+	return 1;
+
+}
+
+int t3_realloc() {
+	s_print("running t2_defrag");
+
+	void *p1 = malloc(100);
+	void *p2 = malloc(100);
+	void *p3 = malloc(100);
+	void *p4 = malloc(100);
+	print_list();
+	realloc(p1, 500);
+	print_list();
+	free(p4);
+	print_list();
+	realloc(p3, 150);
+	print_list();
+
+	p1 = realloc(NULL, 1024);
+	realloc(p1, 0);
+	print_list();
+
+}
+
+void p_print(void *p) {
+	char str[80] = {"\0"};
+	int msg_size = 80;
+
+	snprintf(str, msg_size, "%p\n\r", p);
+	write(STDOUT_FILENO, str, msg_size);
+}
+
+void s_print(char *s) {
+	char str[80] = {"\0"};
+	int msg_size = 80;
+
+	snprintf(str, msg_size, "%s\n\r", s);
+	write(STDOUT_FILENO, str, msg_size);
 }
